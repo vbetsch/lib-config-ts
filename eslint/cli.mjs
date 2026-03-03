8dc38120c87019028adb3b2eb39205e9a8cf326d
vbetsch <vic.betsch@gmail.com>
2026-03-03 18:06:52 +0100

    feat: update cli config

--- eslint/cli.mjs
+++ eslint/cli.mjs
@@ -2,18 +2,18 @@ import baseConfig from './base.mjs';
 import globals from 'globals';
 
 export default [
-  ...baseConfig, // On importe toutes tes règles de base (naming, unused vars, etc.)
+  ...baseConfig,
   {
     languageOptions: {
       globals: {
-        ...globals.node, // On s'assure que 'process', '__dirname', etc. sont reconnus
+        ...globals.node,
       },
     },
     rules: {
-      // Règles spécifiques au développement CLI
-      'no-console': 'off', // Indispensable pour une CLI !
-      'no-process-exit': 'off', // Souvent nécessaire dans les scripts de sortie
-      '@typescript-eslint/no-var-requires': 'off', // Parfois utile pour importer du JSON ou des vieux modules
+      'no-console': 'off',
+      'no-process-exit': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      'prefer-const': 'warn',
     },
   },
 ];
