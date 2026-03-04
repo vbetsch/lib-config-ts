fcd17e11c01afbae854b8105b0b470963ebf31c4
vbetsch <vic.betsch@gmail.com>
2026-03-04 17:46:18 +0100

    test: remove displayName in CLI

--- src/jest/cli.mjs
+++ src/jest/cli.mjs
@@ -2,7 +2,6 @@ import base from './base.mjs';
 
 export default {
   ...base,
-  displayName: 'CLI (after)',
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
 };
