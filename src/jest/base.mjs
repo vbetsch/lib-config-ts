c82620114f61fb339bc734291adbe8debcbc6273
vbetsch <vic.betsch@gmail.com>
2026-03-04 17:38:30 +0100

    fix: remove modulePresets

--- src/jest/base.mjs
+++ src/jest/base.mjs
@@ -2,7 +2,6 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
-  modulePresets: ["ts-jest"],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
   },
