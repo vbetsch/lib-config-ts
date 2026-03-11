e01f1b169b78afe3428fb375defbf79acdcf45fb
vbetsch <71458556+vbetsch@users.noreply.github.com>
2026-03-11 18:46:05 +0100

    feat: Create Jest configuration (#27)
    * feat: create jest configuration base
    
    * feat: improve collectCoverageFrom
    
    * fix: move noEmit
    
    * feat: export jest configs
    
    * fix: require replaced by import
    
    * fix: add package jest-environment-jsdom
    
    * fix: remove moduleFileExtensions
    
    * fix: roots
    
    * docs: order and docstrings

--- src/dynamic/jest/cli.mjs
+++
@@ -1,7 +0,0 @@
-import base from './base.mjs';
-
-export default {
-  ...base,
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
-};
