7124a99e32a0e8aad7adeaaf277644cbe050aac2
vbetsch <vic.betsch@gmail.com>
2026-04-03 21:18:16 +0200

    test: change index method

--- src/packages/config-eslint/src/index.mjs
+++ src/packages/config-eslint/src/index.mjs
@@ -1,9 +1,10 @@
-import base from './modules/base.mjs';
-import jsx from './modules/jsx.mjs';
-import naming from './modules/naming.mjs';
-import prettier from './modules/prettier.mjs';
-import spellcheck from './modules/spellcheck.mjs';
-import strict from './modules/strict.mjs';
+export {default as base} from './modules/base.mjs';
+export {default as jsx} from './modules/jsx.mjs';
+export {default as naming} from './modules/naming.mjs';
+export {default as prettier} from './modules/prettier.mjs';
+export {default as spellcheck} from './modules/spellcheck.mjs';
+export {default as strict} from './modules/strict.mjs';
 
-export {base, jsx, naming, prettier, spellcheck, strict};
+import base from './modules/base.mjs';
+
 export default base;
