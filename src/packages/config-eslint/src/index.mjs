be1a32ba762ff87075af69efd2303c78a7024e1d
vbetsch <vic.betsch@gmail.com>
2026-04-03 21:05:44 +0200

    feat: create index

---
+++ src/packages/config-eslint/src/index.mjs
@@ -0,0 +1,9 @@
+import base from './modules/base.mjs';
+import jsx from './modules/jsx.mjs';
+import naming from './modules/naming.mjs';
+import prettier from './modules/prettier.mjs';
+import spellcheck from './modules/spellcheck.mjs';
+import strict from './modules/strict.mjs';
+
+export {base, jsx, naming, prettier, spellcheck, strict};
+export default base;
