2a81040cf532c6a4798bc9e9c99e9dc6c2d59b2c
vbetsch <vic.betsch@gmail.com>
2026-05-07 18:37:09 +0200

    docs: setupFilesAfterEnv

--- src/packages/config-jest/src/dom.mjs
+++ src/packages/config-jest/src/dom.mjs
@@ -1,5 +1,6 @@
 export default {
   /** The test environment that will be used for testing */
   testEnvironment: 'jsdom',
+  /** A list of paths to modules that run some code to configure or set up the testing framework */
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
 };
