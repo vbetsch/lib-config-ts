0a02550794bb4004244e56ebf76faca2b348dcf4
vbetsch <71458556+vbetsch@users.noreply.github.com>
2026-05-07 19:17:24 +0200

    fix: Next environment (#84)
    * fix: Add '@testing-library/jest-dom' in setupFilesAfterEnv of dom jest config
    
    * docs: setupFilesAfterEnv
    
    * feat: add identity-obj-proxy to catch style files
    
    * refactor: move identity-obj-proxy in dom jest module
    
    * fix: add @testing-library/react in jest optional dependencies
    
    * docs: improve docstrings in jest config
    
    * fix: typo
    
    * fix: version

--- src/packages/config-jest/src/dom.mjs
+++ src/packages/config-jest/src/dom.mjs
@@ -1,4 +1,10 @@
 export default {
+  /** Allow styles files imports in jsx */
+  moduleNameMapper: {
+    '^.+\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  /** Required for jsx testing */
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
   /** The test environment that will be used for testing */
   testEnvironment: 'jsdom',
 };
