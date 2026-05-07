b088a417029aa80bedc498bc28d9bfa8e5881ee3
vbetsch <vic.betsch@gmail.com>
2026-05-07 19:10:55 +0200

    docs: improve docstrings in jest config

--- src/packages/config-jest/src/dom.mjs
+++ src/packages/config-jest/src/dom.mjs
@@ -1,9 +1,9 @@
 export default {
-  /** A map from regular expressions to module names or to arrays of module names that allow to stub out resources */
+  /** Allow styles files import in jsx */
   moduleNameMapper: {
     '^.+\\.(css|sass|scss)$': 'identity-obj-proxy',
   },
-  /** A list of paths to modules that run some code to configure or set up the testing framework */
+  /** Required for jsx testing */
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   /** The test environment that will be used for testing */
   testEnvironment: 'jsdom',
